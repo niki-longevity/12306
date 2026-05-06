71cb68596bfe07c203e75ccba2a2f69fb1f5c37a
niezhi <niezhi@163.com>
2026-05-06 13:57:08 +0800

    新增：PassengerMapper

---
+++ user-service/src/main/java/com/project/user/mapper/PassengerMapper.java
@@ -0,0 +1,9 @@
+package com.project.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.project.common.pojo.entity.Passenger;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PassengerMapper extends BaseMapper<Passenger> {
+}
